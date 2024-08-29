cf5b9d3293e45ebe693ed17d4c6cfd6adae54301
SanjayDwarapureddy <dwarapureddysanjay03@gmail.com>
2024-08-29 19:50:15 +0530

    Add files via upload

---
+++ view.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Bocor Bootstrap Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/f.png" rel="icon">
+  <link href="assets/img/f.png" rel="apple-touch-icon">
+
+  <!-- Bootstrap CSS File -->
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+  
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+ <section id="portfolio" class="portfolio section-bg">
+  <div class="container">
+
+    <h2 data-aos="fade-in">Portfolio</h2>
+    <div>
+<a href="index.html" style="float: right;">Back</a>
+</div>
+
+    <div class="row portfolio-container" data-aos="fade-up">
+      <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+        <div class="portfolio-wrap">
+          <img src="assets/img/portfolio/1.png" class="img-fluid" alt="">
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+        <div class="portfolio-wrap">
+          <img src="assets/img/portfolio/2.png" class="img-fluid" alt="">
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+        <div class="portfolio-wrap">
+          <img src="assets/img/portfolio/3.png" class="img-fluid" alt="">
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+        <div class="portfolio-wrap">
+          <img src="assets/img/portfolio/4.png" class="img-fluid" alt="">
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+        <div class="portfolio-wrap">
+          <img src="assets/img/portfolio/5.png" class="img-fluid" alt="">
+        </div>
+      </div>
+
+      <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+        <div class="portfolio-wrap">
+          <img src="assets/img/portfolio/6.png" class="img-fluid" alt="">
+        </div>
+      </div>
+      
+      <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/7.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/8.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/9.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/10.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/11.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/12.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/13.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/14.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/15.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/16.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/17.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/18.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/19.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/20.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/21.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/22.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/23.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/24.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/25.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/26.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/27.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/28.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/29.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/30.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/31.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/32.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/33.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/34.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/35.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+              <img src="assets/img/portfolio/36.png" class="img-fluid" alt="">
+            </div>
+           </div>
+
+      
+    </div>
+  </div>
+</section>
+
+<!-- jQuery, Popper.js, and Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+<script src="assets/js/main.js"></script>
+</body>
+</html>
